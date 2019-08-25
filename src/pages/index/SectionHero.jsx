471f077b5ea8ae7dc5a2753aa290ae04f6ced9da
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-08-25 17:13:59 -0300

    Section Hero MD

--- src/pages/index/SectionHero.jsx
+++ src/pages/index/SectionHero.jsx
@@ -34,6 +34,7 @@ const SectionHeroStyled = styled.section`
 
   .logos {
     text-align: right;
+    padding-right: 5vw;
   }
 
   .logo {
@@ -48,6 +49,44 @@ const SectionHeroStyled = styled.section`
     position: relative;
     top: -3.5rem;
     opacity: 0.9;
+  }
+
+  @media (min-width: 480px) {
+    .perfil-card {
+      padding: 2.2rem;
+    }
+
+    .logos {
+      padding-right: 10vw;
+    }
+
+    .perfil-image {
+      max-width: 30vw;
+      top: -6.5rem;
+      left: 2.2rem;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .wrapper {
+      margin: 0 auto;
+      position: relative;
+      max-width: 720px;
+    }
+    .perfil-card {
+      padding: 4rem 0.5rem 4rem 12rem;
+    }
+
+    .subtitle {
+      margin-bottom: 0;
+    }
+
+    .perfil-image {
+      position: absolute;
+      top: 4rem;
+      left: 0.5rem;
+      max-height: 220px;
+    }
   }
 
   @media (min-width: 1200px) {
@@ -66,27 +105,30 @@ const SectionHero = ({
 }) => (
   <SectionHeroStyled className="SectionHero">
     <Container>
-      <div className="perfil-card">
-        <div className="title">
-          Hi I'm
-          {" "}
-          <strong>Pedro frohmut</strong>
-          {" "}
-A Full-Stack developer
+      <div className="wrapper">
+        <div className="perfil-card">
+          <div className="title">
+            Hi I'm
+            {" "}
+            <strong>Pedro frohmut</strong>
+            ll-Stack developer
+          </div>
+          <div className="subtitle">
+            Making scalable web apps with ReactJS and AspNetCore. Good looking
+            and grow-frendly user interfaces as front end with a cross-platform
+            and high-performance backend
+          </div>
+          <div className="logos">
+            <img className="react-logo logo" src={reactUrl} alt={reactTitle} />
+            <img
+              className="aspnetcore-logo logo"
+              src={dotnetcoreUrl}
+              alt={dotnetcoreTitle}
+            />
+          </div>
         </div>
-        <div className="subtitle">
-          Making scalable web apps with ReactJS and AspNetCore
-        </div>
-        <div className="logos">
-          <img className="react-logo logo" src={reactUrl} alt={reactTitle} />
-          <img
-            className="aspnetcore-logo logo"
-            src={dotnetcoreUrl}
-            alt={dotnetcoreTitle}
-          />
-        </div>
+        <img className="perfil-image" src={perfilUrl} alt={perfilTitle} />
       </div>
-      <img className="perfil-image" src={perfilUrl} alt={perfilTitle} />
     </Container>
   </SectionHeroStyled>
 )
